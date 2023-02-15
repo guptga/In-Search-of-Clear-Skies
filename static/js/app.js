04f98162cd0fbc9a9e3a64675c26cd34048e46aa
Solarnite <chuthieng@gmail.com>
2023-02-14 21:18:06 -0500

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,39 @@
+// URL
+const url = ""
+
+// Chart Function
+function plot(){
+
+    var trace1 = {
+        x: ,
+        y: ,
+        text: ,
+        type: 
+    };
+
+    var data = [trace1];
+
+    var layout = {
+        title: ,
+        xaxis: ,
+        yaxis:
+    };
+
+    Plotly.newPlot('', trace1, layout);
+}
+
+// Gauge Function
+
+// Heatmap
+
+// Drop down menu
+function init() {
+    
+}
+
+function option_change(newSample) {
+    plot(newSample);
+
+}
+
+init();
