61e93884466334ccff5d0703aaf2991bdb111d00
Solarnite <chuthieng@gmail.com>
2023-02-16 20:24:07 -0500

    Initial commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,42 @@
-// URL
-const url = ""
+ /*async function populate() {
+    // URL
+    const url = "https://github.com/guptga/Project-3-In-Search-of-Clear-Skies/blob/main/sample_AQI_call.json";
+    const request = new Request(url);
+    const response = await fetch(request);
+    const aqi = await response.json();
+}*/
 
+function getData(latitude, longitude) {
+    let url = "https://github.com/guptga/Project-3-In-Search-of-Clear-Skies/blob/main/sample_AQI_call.json";
+    fetch(url)
+    .then(function(response) {
+        let data = response.json();
+        return data;
+    })
+    .then(function(data) {
+        air.index = data.data.aqi;
+        air.city = data.data.city.name;
+    })
+    .then(function(){
+        showData();
+    })
+}
+
+
+/*
+
+const url = "https://github.com/guptga/Project-3-In-Search-of-Clear-Skies/blob/main/sample_AQI_call.json";
+function plot(id) {
+    d3.json(url).then(function(data)) {
+        var samples = data.samples;
+        var resultArray = samples.filter(sampleObj => sampleObj.id == id);
+        var result = resultArray[0];
+        var aqi = result.aqi;
+        var attributions = 
+        var 
+        console.log(aqi, city, idx)
+    
+}
 // Chart Function
 function plot(){
 
@@ -37,3 +73,5 @@ function option_change(newSample) {
 }
 
 init();
+*/
+
