572c5ed10ea9327af994e03cb5aa7748b7766e56
kidstonb <113933033+kidstonb@users.noreply.github.com>
2023-02-20 20:46:44 -0500

    Questionable heatmap, added markers

--- static/js/app_d3_starter.js
+++ static/js/app_d3_starter.js
@@ -1,41 +1,124 @@
 coordinates = ['43.591706','-81.639478','45.747761','-76.306641'];
+// coordinates = ['28.491626','-126.741920','67.931333','-64.976622'];
 // const url_map = `https://api.waqi.info/map/bounds?token=${aqi_key}&latlng=43.591706,-81.639478,45.747761,-76.306641`;
 const url_map = `https://api.waqi.info/map/bounds?token=${aqi_key}&latlng=${coordinates[0]},${coordinates[1]},${coordinates[2]},${coordinates[3]}`;
-
-// map object
-var map = L.map("map", {
-    center: [43.6532,-79.3832],
-    zoom: 12
-}); //.setView([44,-77],2);
-
-// Adding the tile layer
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-// var basicBackground = L.tileLayer(
-//     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
-//     maxZoom: 6
-// }).addTo(map);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
 
 // function to add heat to map
 function heat(response){
     heatArray = [];
+    heatAQI = [];
     for (var i = 0; i < response.data.length; i++) {
         // var location = data[i].location;
         // if (location) {
-          heatArray.push([response.data[i].lat, response.data[i].lon, response.data[i].aqi]);
+        if (Number.isNaN(response.data[i].aqi)){
+            console.log("Omission at " + response.data[i].lat + ", " + response.data[i].lon + " due to NaN");
+        }
+        else{
+        //   heatArray.push({"lat": response.data[i].lat, 
+        //   "lon": response.data[i].lon, 
+        //   "aqi": 80*Math.sqrt(response.data[i].aqi)});
+          heatArray.push([response.data[i].lat,response.data[i].lon,5000*response.data[i].aqi]); //5*Math.sqrt(response.data[i].aqi)]);  //,0.2*(response.data[i].aqi**2)]);
+        //   heatAQI.push(90*Math.sqrt(response.data[i].aqi));
+
+        // response.data[i].aqi = 500*(response.data[i].aqi)**(-2);
+        }
         // }
       }
     // console.log(heatArray);
 
-    var heat = L.heatLayer(heatArray, {radius: 100, blur:35}).addTo(map);
+    // For leaftlet-heatmap.js
+    var heat = new HeatmapOverlay({
+        "radius": 0.5,
+        // "blur": 25,
+        "maxOpacity": .4,
+        "scaleRadius": true,
+        // "useLocalExtrema": true,
+        latField: 'lat',
+        lngField: 'lon',
+        valueField: 'aqi'
+    }).addTo(map);
+    heat.setData(response);
+    heatLayer = {"AQI Heatmap": heat};
+
+    // var heat = L.heatLayer(heatArray, {
+    //     'radius': 35,
+    //     'blur': 40,
+    //     'max': 100,
+    //     gradient: {0.1: 'green',0.5: 'yellow', 1.0: 'red'}
+    // }).addTo(map);
+
+    L.control.layers({},heatLayer).addTo(map);
+
+    // return heatLayer;
+}
+
+// function createMap(layer1, layer2){
+// map object
+var map = L.map("map", {
+    center: [43.6532,-79.3832],
+    zoom: 7, // 5 previously
+    // layers: [heat]
+}); //.setView([44,-77],2);
+
+// Adding the tile layer
+baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="htts://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: 2,
+    maxZoom: 12 // 8 for large map?
+}).addTo(map);
+
+// Add map layers to control
+// L.control.layers(heat,{collasped: false}).addTo(map);
+// L.control.layers({},layer1, layer2).addTo(map);
+
+// }
+
+function addMarkers(data){
+
+    markerArray = [];
+
+    for (var i = 0; i < data.data.length; i++) {
+        var aqi_object = data.data[i].aqi;
+        var latlng = [data.data[i].lat, data.data[i].lon];
+
+        if (aqi_object) {
+            var marked = L.marker(latlng, {
+
+            })
+            // .bindPopup("<h3>" + aqi_object + "</h3>")
+            .bindTooltip(aqi_object,{
+                // permanent: true
+                // sticky: true
+                opacity: 0.7,
+                direction: 'top'
+            })
+            .addTo(map);
+            markerArray.push(marked);
+        }
+
+    }
+    // L.control.layers(L.layerGroup(markerArray)).addTo(map);
+    // return L.layerGroup(markerArray);
 }
 
 d3.json(url_map).then(function(data){
+    setTimeout(function(){
+    },1000);
+    // setTimeout(function(){map.invalidateSize(true);},500);
     console.log(data);
+    // heatGroup = heat(data);
+    
+    // console.log(heatGroup);
+    // heat_layer = L.layerGroup(heatGroup);
+    // overlayHeat = {
+    //     "AQI Gradient": heat_layer
+    // };
+    
     heat(data);
+    addMarkers(data);
+    // var layer1 = heat(data);
+    // var layer2 = addMarkers(data);
+    // createMap(layer1, layer2);
+
+    
 });
